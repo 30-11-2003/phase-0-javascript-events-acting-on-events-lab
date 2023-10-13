559ef2da4206244257489e7b920161842dc3ac71
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-10-13 18:31:42 +0300

    updated

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+const dodger =document.getElementById("dodger");
+function moveDodgerRight() {
+    const leftNumbers=dodger.style.left.replace("px","");
+    const left =parseInt(leftNumbers,10);
+
+    if (left>0) {
+        dodger.style.left = `${left+1}px`;
+    }
+}
+
+document.addEvevntListener ("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+}
